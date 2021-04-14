2aa4c8971a9fd5a174dfe4c9a2dd50a54334e429
Jan Góral <jan.goral112@gmail.com>
2021-04-14 14:26:23 +0200

    Added home, footer and top-bar components, basic routes and template for app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
