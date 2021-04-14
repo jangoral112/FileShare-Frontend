258b38cd38fca8607e28f12ee5a37cbf0d318abc
Jan Góral <jan.goral112@gmail.com>
2021-04-14 20:00:20 +0200

    Replaced header with home-header and dashboard-header and added basic css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HomeHeaderComponent } from './components/header/home-header/home-header.component';
+import { DashboardHeaderComponent } from './components/header/dashboard-header/dashboard-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HomeHeaderComponent,
+    DashboardHeaderComponent
   ],
   imports: [
     BrowserModule,
