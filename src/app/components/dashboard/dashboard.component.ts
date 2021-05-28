c4222827af52c9bb9d83a927e8d6e3ae79189c37
Jan Góral <jan.goral112@gmail.com>
2021-05-28 15:52:11 +0200

    Added list of public files in main dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FileTableComponent} from '../files/file-table/file-table.component';
+import {FileService} from '../../services/file.service';
+import {SessionStorageService} from '../../services/session-storage.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild(FileTableComponent)
+  fileTable: FileTableComponent;
+
+  fileService: FileService;
+
+  sessionStorageService: SessionStorageService;
+
+  constructor(fileService: FileService, sessionStorageService: SessionStorageService) {
+    this.fileService = fileService;
+    this.sessionStorageService = sessionStorageService;
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.fileTable.setFilesMetadata(this.fileService.getFilesMetadata(null, true));
   }
 
 }
